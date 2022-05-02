a5dba7e9a82c5a3b69aac1cfec538493104d9868
kobi krasnoff <krasnoff.kobi@gmail.com>
2022-05-02 21:50:01 +0300

    add one frame skip

--- src/FramedVideo/FramedVideo.js
+++ src/FramedVideo/FramedVideo.js
@@ -1,22 +1,29 @@
 import './FramedVideo.module.css';
-import { useRef, useEffect } from 'react';
+import { useRef } from 'react';
 
 function FramedVideo(props) {
     const videoComponent = useRef(null);
+    let frameTime = 1/ 25;
 
-    useEffect(() => {
-      videoComponent.current.oncanplaythrough = () => {
-        console.log("Can play through video without stopping");
-        console.log("duration", videoComponent.current.duration);
-      };
-    }, []);
-
-    const play = () => {
-      videoComponent.current.play();
+    const forward = () => {
+      OneFrame(false);
     }
 
-    const pause = () => {
-      videoComponent.current.pause();
+    const backword = () => {
+      OneFrame(true);
+    }
+
+    const OneFrame = (direction) => {
+      if (videoComponent.current.paused) { //or you can force it to pause here
+        if (direction) { //left arrow
+            //one frame back
+            videoComponent.current.currentTime = Math.max(0, videoComponent.current.currentTime - frameTime);
+        } else if (!direction) { //right arrow
+            //one frame forward
+            //Don't go past the end, otherwise you may get an error
+            videoComponent.current.currentTime = Math.min(videoComponent.current.duration, videoComponent.current.currentTime + frameTime);
+        }
+      }  
     }
   
     return (
@@ -25,8 +32,8 @@ function FramedVideo(props) {
             <source src={props.src} type="video/mp4" />
             Your browser does not support HTML video.
           </video>
-          <button onClick={() => play()}>Play</button>
-          <button onClick={() => pause()}>pause</button>
+          <button onClick={() => forward()}>forward</button>
+          <button onClick={() => backword()}>backword</button>
         </figure>
     );
 }
